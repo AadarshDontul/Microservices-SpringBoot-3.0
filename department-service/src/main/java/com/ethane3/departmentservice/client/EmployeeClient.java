8e20a56f42dc6a42e8c04ca68de4d22d99885507
Aadarsh-D <aadarshdontul03@gmail.com>
2025-01-03 19:50:53 +0530

    WebClient and HttpServiceProxyFactory is used for configuration of Employee client in Department serivice to call API of Employee service in Department service

--- department-service/src/main/java/com/ethane3/departmentservice/client/EmployeeClient.java
+++ department-service/src/main/java/com/ethane3/departmentservice/client/EmployeeClient.java
@@ -1,11 +1,9 @@
 package com.ethane3.departmentservice.client;
 
 import com.ethane3.departmentservice.model.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
-
 import java.util.List;
 
 @HttpExchange
--- department-service/src/main/java/com/ethane3/departmentservice/controller/DepartmentController.java
+++ department-service/src/main/java/com/ethane3/departmentservice/controller/DepartmentController.java
@@ -1,9 +1,11 @@
 package com.ethane3.departmentservice.controller;
 
+import com.ethane3.departmentservice.client.EmployeeClient;
 import com.ethane3.departmentservice.model.Department;
 import com.ethane3.departmentservice.repository.DepartmentRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -37,6 +39,20 @@ public class DepartmentController {
     public Department findById(@PathVariable Long id){
         LOGGER.info("Department find :{}",id);
         return departmentRepository.findById(id);
+    }
+
+    @Autowired
+    private EmployeeClient employeeClient;
+
+    @GetMapping("/with-employees")
+    public List<Department> findAllWithEmployees(){
+        LOGGER.info("Department find with-employees");
+        List<Department> departments = departmentRepository.findAll();
+        departments.forEach(department -> department.setEmployeeList(
+                employeeClient.getEmployeeByDepartment(department.getId())
+        ));
+
+        return departments;
     }
 
 }
